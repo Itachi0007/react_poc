f7f65f16b4f21d0804fcfca4ad782e22b96d3a3e
Itachi0007 <ahmer.mehmood191@gmail.com>
2022-03-02 12:56:57 +0530

    intro to jsx

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Page />
+		</div>
+	);
+}
+
+function Page() {
+	return (
+		<div className="header" id="header">
+			<img src={logo} width="150px"></img>
+			<h1>Fun facts about React</h1>
+			<ul className="facts list">
+				<li>It is declarative</li>
+				<li>It is imperative</li>
+				<li>It is a hireable skill</li>
+			</ul>
+		</div>
+	);
 }
 
 export default App;
